a03944dd0c12fc7131d85d52aceff9e750caf6a4
Thiago Nogueira <thiago.droz@gmail.com>
2018-08-04 15:22:29 -0300

    New solutions to arrays-ds

---
+++ DataStructures/Arrays/arrays-ds.cs
@@ -0,0 +1,40 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+    static int[] reverseArray(int[] a) {
+        int[] newArray = new int[a.Length];
+        
+        for(var i = 0; i < a.Length; i++) {
+            newArray[i] = a[a.Length - i - 1];
+        }
+
+        return newArray;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int[] res = reverseArray(arr);
+
+        textWriter.WriteLine(string.Join(" ", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
--- DataStructures/Arrays/arrays-ds.py
+++ DataStructures/Arrays/arrays-ds.py
@@ -12,3 +12,17 @@ for x in range(len(arr) - 1, -1, -1):
   result += str(arr[x]) + " "
 
 print(result)
+
+# ------------ New Solution ------------------
+#!/bin/python3
+
+
+def reverseArray(a):
+  return [a[len(a) - x - 1] for x in range(len(a))]
+
+
+arr_count = int(input())
+arr = list(map(int, input().split()))
+res = reverseArray(arr)
+
+print(' '.join(map(str, res)))
