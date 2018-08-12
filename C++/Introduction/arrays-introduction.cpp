26848874875c1975f94f563ed013d17ec69fdc43
Thiago Nogueira <thiago.droz@gmail.com>
2018-08-12 13:44:01 -0300

    C++ Arrays introduction

---
+++ C++/Introduction/arrays-introduction.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int len;
+  cin >> len;
+
+  int arr[len];
+  for (int i = 0; i < len; i++)
+  {
+    cin >> arr[i];
+  }
+
+  for (int i = len - 1; i >= 0; i--)
+  {
+    cout << arr[i] << " ";
+  }
+
+  return 0;
+}
