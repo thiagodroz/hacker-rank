c539096a53db3089f21dec62afe9400626056467
Thiago Nogueira <thiago.droz@gmail.com>
2021-06-04 10:37:53 -0300

    Adding support to TS and adding Interview Warm-Up Challenges starting with sock-merchant

---
+++ InterviewPreparationKit/WarmUpChallenges/sock-merchant.ts
@@ -0,0 +1,69 @@
+'use strict'
+
+import { WriteStream, createWriteStream } from 'fs'
+process.stdin.resume()
+process.stdin.setEncoding('utf-8')
+
+let inputString: string = ''
+let inputLines: string[] = []
+let currentLine: number = 0
+
+process.stdin.on('data', function (inputStdin: string): void {
+  inputString += inputStdin
+})
+
+process.stdin.on('end', function (): void {
+  inputLines = inputString.split('\n')
+  inputString = ''
+
+  main()
+})
+
+function readLine(): string {
+  return inputLines[currentLine++]
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n: number, ar: number[]): number {
+  // Write your code here
+  const colors: Record<string, number> = {}
+
+  ar.forEach(sock => {
+    if (colors[sock] === undefined) {
+      colors[sock] = 1
+    } else {
+      colors[sock] += 1
+    }
+  })
+
+  const result = Object.keys(colors).reduce((result, k) => {
+    return result + Math.floor(colors[k] / 2)
+  }, 0)
+
+  return result
+}
+
+function main() {
+  const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH'])
+
+  const n: number = parseInt(readLine().trim(), 10)
+
+  const ar: number[] = readLine()
+    .replace(/\s+$/g, '')
+    .split(' ')
+    .map(arTemp => parseInt(arTemp, 10))
+
+  const result: number = sockMerchant(n, ar)
+
+  ws.write(result + '\n')
+
+  ws.end()
+}
